870abf25b67dbfba1b002b91a443fca186690c47
anshul543 <69334649+anshul543@users.noreply.github.com>
2020-11-23 11:28:31 -0800

    Add files via upload

---
+++ LCM calculator.py
@@ -0,0 +1,13 @@
+def LCM(x,y):
+	if x > y:
+		smaller = y
+	else:
+		smaller = x
+	for i in range(1,smaller+1):
+		if ((x % i == 0) and (y % i == 0)):
+			hcf = i
+			lcm = x * y / hcf
+	return lcm
+
+    
+        
